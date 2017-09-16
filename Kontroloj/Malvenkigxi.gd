d7c2eaf33bc9a48e4d402f67dd9254ed1a2a4917
hsn6 <hsn6@openmailbox.org>
2017-09-16 13:35:03 +0430

    restitajxoj el 306eb35

--- Kontroloj/Malvenkigxi.gd
+++ Kontroloj/Malvenkigxi.gd
@@ -4,7 +4,7 @@ var agordejo = "user://agordejo.cfg"
 onready var Agordejo = ConfigFile.new()
 
 func _ready():
-	get_tree().set_auto_accept_quit(true)
+	get_tree().set_auto_accept_quit(false)
 	Agordejo.load(agordejo)
 	get_node("Poentaro").set_text(str(Tutmonda.poentaro))
 	if Tutmonda.poentaro == 1 or Tutmonda.poentaro == 0:
@@ -14,6 +14,10 @@ func _ready():
 	elif Tutmonda.epoko == "malnova" and Tutmonda.poentaro > Agordejo.get_value("Rekordoj", "malnova_epoko", 0):
 		Agordejo.set_value("Rekordoj", "malnova_epoko", Tutmonda.poentaro)
 	Agordejo.save(agordejo)
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		get_tree().change_scene("res://Kontroloj/Niveloj.tscn")
 
 func _on_Reen_pressed():
 	get_tree().change_scene("res://Kontroloj/Niveloj.tscn")
--- Kontroloj/Venkigxi.gd
+++ Kontroloj/Venkigxi.gd
@@ -4,16 +4,18 @@ var agordejo = "user://agordejo.cfg"
 onready var Agordejo = ConfigFile.new()
 
 func _ready():
-	get_tree().set_auto_accept_quit(true)
-
+	get_tree().set_auto_accept_quit(false)
 	Agordejo.load(agordejo)
-
 	get_node("Poentaro").set_text(str(Tutmonda.poentaro))
 	if Tutmonda.epoko == "nova":
 		Agordejo.set_value("Rekordoj", "nova_epoko", Tutmonda.poentaro)
 	elif Tutmonda.epoko == "malnova":
 		Agordejo.set_value("Rekordoj", "malnova_epoko", Tutmonda.poentaro)
 	Agordejo.save(agordejo)
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		get_tree().change_scene("res://Kontroloj/Niveloj.tscn")
 
 func _on_Reen_pressed():
 	get_tree().change_scene("res://Kontroloj/Niveloj.tscn")
