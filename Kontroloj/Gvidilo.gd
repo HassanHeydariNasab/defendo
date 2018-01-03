b1f154e4f1a031743faef1233c9087c0763c99d6
hsn6 <hsn6@openmailbox.org>
2018-01-03 17:10:15 +0330

    aldonigxis litecoin. Closes #50

--- Kontroloj/Gvidilo.gd
+++ Kontroloj/Gvidilo.gd
@@ -16,5 +16,8 @@ func _on_Fontkodo_pressed():
 func _on_Atribuoj_pressed():
 	OS.shell_open("https://github.com/HassanHeydariNasab/defendo/blob/master/ATRIBUOJ.md")
 
-func _on_Donaci_pressed():
+func _on_Bitcoin_pressed():
 	OS.shell_open("bitcoin:16D7Nroenpx4QDNqfq3Js7sdAVhew2NzGp")
+
+func _on_Litecoin_pressed():
+	OS.shell_open("litecoin:LXGhvqVGAs6rpxDPagF17QvPKDQJR1ngmj")
